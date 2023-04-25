0a49fa25454e7f704e14b27ee132b5913ac39a2b
Khan-Desktop <kxisystem441@gmail.com>
2023-04-25 14:46:37 -0400

    - changed file structure for github pages

--- twine-build.js
+++ twine-build.js
@@ -1,17 +1,17 @@
-const fs = require('fs');
+import { readFileSync, writeFileSync } from 'fs';
 
-const infoFile = fs.readFileSync("twine/template.json", 'utf8');
+const infoFile = readFileSync('twine/template.json', 'utf8');
 const info = JSON.parse(infoFile);
 
-const templateFile = fs.readFileSync("dist/index.html", 'utf8');
+const templateFile = readFileSync('dist/index.html', 'utf8');
 info.source = templateFile;
 
-let versionString = process.argv.find(x => x.includes("v="));
-if (versionString != null){
+let versionString = process.argv.find(x => x.includes('v='));
+if (versionString != null) {
     info.version = versionString.slice(2);
 }
 
-let format = "window.storyFormat(" + JSON.stringify(info) + ");";
-let filename = "yarntwiner_" + info.version + ".js"
-fs.writeFileSync( "release/" + filename, format);
-console.log("build complete at release/" + filename);
+let format = `window.storyFormat(${JSON.stringify(info)});`;
+let filenamedir = `docs/release/yarntwiner_${info.version}.js`;
+writeFileSync(filenamedir, format);
+console.log('build complete at ' + filenamedir);
