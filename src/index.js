2f9f0b8183d6cdc2a1393c9392c6bf7cfb78dd61
Khan-Desktop <kxisystem441@gmail.com>
2023-04-26 18:14:39 -0400

    - minor rename in conversion.js - added comments - made builds for 0.2.0 (+2 squashed commit)
    Squashed commit:
    
    [4e09c49] - made test build script
    - optimized twine tag parsing
    - used inner text for twine node parsing
    - changed layout of html page
    
    [85fd3e1] -added logo

--- src/index.js
+++ src/index.js
@@ -2,18 +2,24 @@ import './main.css'
 import * as convert from './convert.js'
 
 window.addEventListener('load', async function() {
-    const TWPassageData = document.querySelectorAll('tw-storydata > tw-passagedata');
-    const NodeData = convert.TWPassagesToNodes(Object.values(TWPassageData));
+    // grab story data info
+    const TWStoryData = document.querySelectorAll('tw-storydata')[0];
+    // grab passage data info and make it into an array
+    const TWPassageData = Object.values(document.querySelectorAll('tw-storydata > tw-passagedata'));
+    // main conversion process
+    const NodeData = convert.TWPassagesToNodes(TWPassageData);
+    // yarn file process
     const YarnData = convert.NodesToString(NodeData);
     
     const FileOutput = this.document.querySelector('.code');
-    FileOutput.innerHTML = YarnData;
+    FileOutput.innerText = YarnData;
 
-    this.document.querySelector('.download > button').addEventListener('click', async => {
-        downloadFile(FileOutput.innerText, 'output.yarn');
+    this.document.querySelector('.download').addEventListener('click', async => {
+        downloadFile(FileOutput.innerText, TWStoryData.attributes.name.value + '.yarn');
     });
 });
 
+// helper to make a button to download a file
 function downloadFile(data, fileName) {
     let file = new File([data], fileName, { type: 'application/octet-stream' });
     let a = document.createElement('a');
