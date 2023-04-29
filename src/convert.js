15ab699b78fae32dec355887534bc55312b873cf
Khan-Desktop <kxisystem441@gmail.com>
2023-04-29 14:00:33 -0400

    - minor rename in conversion.js - added comments - made builds for 0.2.0

--- src/convert.js
+++ src/convert.js
@@ -1,18 +1,25 @@
+// takes an array of twine story passages and turns them into yarntwiner yarn nodes
 export function TWPassagesToNodes(passageData) {
     return passageData.map(passage => {
-        let isRaw = false;
-        let isIgnore = false;
+        let outputNode = {
+            name: '',
+            headers: [],
+            content: '',
+            raw: false,
+            ignore: false,
+            order: 0,
+        };
+
         let isComment = false;
-        let order = 0;
 
         // sanitize name
         let name = passage.attributes['name'].value;
-        name = name.replace(/[\s\.]/g, '_');
+        outputNode.name = name.replace(/[\s\.]/g, '_');
 
         // tags are lumped with the node attributes
-        let attr = passage.attributes['tags'].value.split(' ')
+        let attr = passage.attributes['tags'].value.split(' ');
         // we parse and sort the twine tags into yarn attributes
-        .reduce((acc, val) => {
+        outputNode.headers = attr.reduce((acc, val) => {
             // if we have no tags
             if (!val) {
                 return acc;
@@ -21,17 +28,17 @@ export function TWPassagesToNodes(passageData) {
             // special options tag
             if (val.includes('@')) {
                 // we want to treat the node contents as raw yarn data
-                if (val === '@raw') { isRaw = true; }
+                if (val === '@raw') { outputNode.raw = true; }
 
                 // comment out every line
                 if (val === '@comment') { isComment = true; }
 
                 // ignore this node for final export
-                if (val === '@ignore') { isIgnore = true; }
+                if (val === '@ignore') { outputNode.isComment = true; }
 
                 // we want to change the order in the file export
                 if (val.includes('@order')) {
-                    order = Number(val.split(':')[1]);
+                    outputNode.order = Number(val.split(':')[1]);
                 }
 
                 // its not a node attribute, we don't add anything
@@ -62,30 +69,24 @@ export function TWPassagesToNodes(passageData) {
 
         // position attribute is similar enough between twine and yarn
         let pos = passage.attributes['position'].value;
-        attr.push({ name: 'position', value: pos});
+        outputNode.headers.push({ name: 'position', value: pos});
 
         // replace [[link]] with <<jump>>
         let inner = passage.innerText;
-        inner = inner.replace(/\[\[(.*?)\]\]/g, (match, target) => {
+        outputNode.content = inner.replace(/\[\[(.*?)\]\]/g, (match, target) => {
             return `<<jump ${target}>>`;
         });
 
         // turn the node contents into comments if we requested it
         if (isComment) {
-            inner = inner.replace(/^/gm, '//');
+            outputNode.content = outputNode.content.replace(/^/gm, '//');
         }
 
-        return {
-            name: name,
-            attributes: attr,
-            content: inner,
-            raw: isRaw,
-            ignore: isIgnore,
-            order: order,
-        };
+        return outputNode;
     });
 }
 
+// turns yarntwiner yarn nodes into a text output
 export function NodesToString(nodes, printIgnore = false) {
     return nodes
     .sort((a, b) => a.order - b.order)
@@ -98,11 +99,11 @@ export function NodesToString(nodes, printIgnore = false) {
             return `${acc}${node.content}\n\n` ;
         }
 
-        let attr = node.attributes.reduce((a, x) => {
+        let hdrs = node.headers.reduce((a, x) => {
             a.push(`${x.name}: ${x.value}`)
             return a;
         }, []);
 
-        return `${acc}title: ${node.name}\n${attr.join('\n')}\n---\n${node.content}\n===\n\n`;
+        return `${acc}title: ${node.name}\n${hdrs.join('\n')}\n---\n${node.content}\n===\n\n`;
     }, '');
 }
--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import './main.css'
 import * as convert from './convert.js'
 
 window.addEventListener('load', async function() {
+    // grab story data info
     const TWStoryData = document.querySelectorAll('tw-storydata')[0];
+    // grab passage data info and make it into an array
     const TWPassageData = Object.values(document.querySelectorAll('tw-storydata > tw-passagedata'));
+    // main conversion process
     const NodeData = convert.TWPassagesToNodes(TWPassageData);
+    // yarn file process
     const YarnData = convert.NodesToString(NodeData);
     
     const FileOutput = this.document.querySelector('.code');
     FileOutput.innerText = YarnData;
 
-    this.document.querySelector('button.download').addEventListener('click', async => {
+    this.document.querySelector('.download').addEventListener('click', async => {
         downloadFile(FileOutput.innerText, TWStoryData.attributes.name.value + '.yarn');
     });
 });
 
+// helper to make a button to download a file
 function downloadFile(data, fileName) {
     let file = new File([data], fileName, { type: 'application/octet-stream' });
     let a = document.createElement('a');
